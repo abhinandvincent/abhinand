634e61e7368327094e204ece6db566647d8d59d6
Abhinand Vincent <abhinandvincent2006@gmail.com>
2026-05-25 13:40:07 +0530

    switched to sqlite for render

--- product/settings.py
+++ product/settings.py
@@ -67,12 +67,8 @@ WSGI_APPLICATION = 'product.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'product',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
