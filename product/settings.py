3a86f8e04de890ff7524d96d7572f6246b349f53
Abhinand Vincent <abhinandvincent2006@gmail.com>
2026-06-21 18:43:44 +0530

    Connect Neon database

--- product/settings.py
+++ product/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 template_dir = os.path.join(BASE_DIR, 'template')
 
@@ -66,10 +67,9 @@ WSGI_APPLICATION = 'product.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
